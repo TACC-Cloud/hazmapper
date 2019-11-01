90f1e120eebb77de85617622045df0c579886e4e
Nathan Franklin <nathan.franklin@gmail.com>
2019-11-01 18:09:24 -0500

    Add ability to add feature asset if there are none

--- src/app/components/assets-panel/assets-panel.component.ts
+++ src/app/components/assets-panel/assets-panel.component.ts
@@ -1,13 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {FeatureCollection} from 'geojson';
 import {GeoDataService} from '../../services/geo-data.service';
-import {AssetFilters, Feature, Project} from '../../models/models';
+import {FeatureAsset, Feature, Project} from '../../models/models';
 import {BsModalRef, BsModalService} from 'ngx-foundation';
 import {ModalFileBrowserComponent} from '../modal-file-browser/modal-file-browser.component';
 import { ModalCreatePointCloudComponent} from '../modal-create-point-cloud/modal-create-point-cloud.component';
 import {ProjectsService} from '../../services/projects.service';
 import {RemoteFile} from 'ng-tapis';
-import {Subject} from 'rxjs';
 import { ScrollableArray } from '../../utils/ScrollableArray';
 
 
@@ -67,7 +66,6 @@ export class AssetsPanelComponent implements OnInit {
     //   this.geoDataService.importFileFromTapis(this.activeProject.id, files);
     // });
   }
-
 
   handleFileInput(files: FileList) {
     // tslint:disable-next-line:prefer-for-of
