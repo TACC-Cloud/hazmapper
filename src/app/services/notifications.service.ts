de9d349c47f5fba70af3fe13282026362c7c19e1
Joe Meiring <jmeiring@tacc.utexas.edu>
2020-03-31 12:14:48 -0500

    polling notifications

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -1,12 +1,50 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import {HttpClient} from '@angular/common/http';
+import {INotification} from '../models/notification';
+import {interval, Observable, ReplaySubject} from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor(private toastr: ToastrService) { }
+  // Interval time to get notifications in millisecs
+  private TIMEOUT = 5000;
+  private environment = environment;
+  private _notifications: ReplaySubject<Array<INotification>> = new ReplaySubject<Array<INotification>>(1);
+  public readonly  notifications: Observable<Array<INotification>> = this._notifications.asObservable();
+
+  constructor(private toastr: ToastrService, private http: HttpClient) {
+    const timer = interval(this.TIMEOUT);
+    timer.subscribe( (next) => {
+      this.getRecent();
+    });
+  }
+
+  getRecent(): void {
+    const baseUrl = this.environment.apiUrl + '/notifications/';
+    const now = new Date();
+    const then = new Date(now.getTime() - this.TIMEOUT);
+
+    this.http.get<Array<INotification>>(baseUrl + `?startDate=${then.toISOString()}`)
+      .subscribe( (notes) => {
+        this._notifications.next(notes);
+        notes.forEach( (note) => {
+          switch (note.status) {
+            case 'success':
+              this.showSuccessToast(note.message);
+              break;
+            case 'error':
+              this.showErrorToast(note.message);
+              break;
+            default:
+              break;
+          }
+        });
+      });
+  }
 
   showSuccessToast(message: string): void {
     this.toastr.success(message);
