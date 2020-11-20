26f76a46156f3427b6905a9fab63761b9f79e62e
duckonomy <contact@duckonomy.com>
2020-11-20 15:55:47 -0600

    Upstream stuff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { ModalConfirmationBodyComponent } from './components/modal-confirmation-body/modal-confirmation-body.component';
 import { UserRowComponent } from './components/user-row/user-row.component';
+import { EditNameInputComponent } from './components/edit-name-input/edit-name-input.component';
 
 @NgModule({
   declarations: [
