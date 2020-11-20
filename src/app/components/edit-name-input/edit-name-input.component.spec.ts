9c720740828ea88023cbae7633da400fc2c2f1d7
duckonomy <contact@duckonomy.com>
2020-11-20 10:25:34 -0600

    Add new component.

---
+++ src/app/components/edit-name-input/edit-name-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditNameInputComponent } from './edit-name-input.component';
+
+describe('EditNameInputComponent', () => {
+  let component: EditNameInputComponent;
+  let fixture: ComponentFixture<EditNameInputComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditNameInputComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditNameInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
