40f46434f66d4fc3753c4368b9d5082248352be9
Nathan Franklin <nathan.franklin@gmail.com>
2020-11-24 15:05:10 -0600

    Add pure cesium

--- src/app/components/map3d/map3d.component.ts
+++ src/app/components/map3d/map3d.component.ts
@@ -17,32 +17,71 @@ import OLCesium from 'ol-cesium';
 export class Map3dComponent implements AfterViewInit {
   olMap: Map;
   ol3d: OLCesium;
+  viewer: Cesium.Viewer;
+  private useOLCesium: boolean;
 
-  constructor() { }
+  constructor() {
+    this.useOLCesium = false;
+  }
 
   ngAfterViewInit() {
-    this.olMap = new Map({
-      target: 'map3d',
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
+    if (this.useOLCesium) {
+      this.olMap = new Map({
+        target: 'map3d',
+        layers: [
+          new TileLayer({
+            source: new XYZ({
+              url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+          }),
+        ],
+        view: new View({
+          center: olProj.fromLonLat([105.2, 39.66]),
+          zoom: 4
+        })
+      });
+
+      this.ol3d = new OLCesium({map: this.olMap, target: 'map3d', layers: []});
+
+      const scene = this.ol3d.getCesiumScene();
+      // tslint:disable-next-line:max-line-length
+      Cesium.Ion.defaultAccessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4Zjg0YWQ0My0wNzU1LTQwODYtYWIxOS1jMjg2YmQ4NTc1OGUiLCJpZCI6MzgzMTQsImlhdCI6MTYwNjIzMTY3OH0.SkWEoWp-Xjfw7kI3kC9fN2ymmQ_zQ4Dd-UDNOKUk98g`;
+      scene.terrainProvider = Cesium.createWorldTerrain();
+
+      // 3d tile sets not showing??
+      scene.primitives.add(new Cesium.Cesium3DTileset({ url: Cesium.IonResource.fromAssetId(5714) }));
+      scene.primitives.add(Cesium.createOsmBuildings());
+
+      this.ol3d.setEnabled(true);
+    } else {
+      this.viewer = new Cesium.Viewer('map3d', {
+        terrainProvider : Cesium.createWorldTerrain(),
+        //Hide the base layer picker
+        baseLayerPicker : false,
+        //Use OpenStreetMaps
+        imageryProvider : new Cesium.OpenStreetMapImageryProvider({
+          url : 'https://a.tile.openstreetmap.org/'
         }),
-      ],
-      view: new View({
-        center: olProj.fromLonLat([105.2, 39.66]),
-        zoom: 4
-      })
-    });
+        // Show Columbus View map with Web Mercator projection
+        mapProjection : new Cesium.WebMercatorProjection()
+      });
+
+      this.viewer.scene.primitives.add(Cesium.createOsmBuildings());
+
+      const redRock = this.viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+        url: `http://cesium.entwine.io/data/red-rocks/tileset.json`
+      }));
+      redRock.style = new Cesium.Cesium3DTileStyle({
+        pointSize: 2
+      });
+      this.viewer.zoomTo(redRock);
+      this.viewer.flyTo(redRock);
 
 
-    this.ol3d = new OLCesium({map: this.olMap});
-    this.ol3d.setEnabled(true);
+      this.viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+        url: `http://cesium.entwine.io/data/nyc-2/tileset.json`
+      }));
 
-    const scene = this.ol3d.getCesiumScene();
-    // tslint:disable-next-line:max-line-length
-    Cesium.Ion.defaultAccessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4Zjg0YWQ0My0wNzU1LTQwODYtYWIxOS1jMjg2YmQ4NTc1OGUiLCJpZCI6MzgzMTQsImlhdCI6MTYwNjIzMTY3OH0.SkWEoWp-Xjfw7kI3kC9fN2ymmQ_zQ4Dd-UDNOKUk98g`;
-    scene.terrainProvider = Cesium.createWorldTerrain();
+    }
   }
 }
