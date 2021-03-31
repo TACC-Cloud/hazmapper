f05146982d686b8708360df1d3354b174502df77
Nathan Franklin <nathan.franklin@gmail.com>
2021-03-31 10:49:02 -0500

    Remove unused code

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import {GeoDataService} from '../../services/geo-data.service';
-// import {Feature} from '../../models/models';
 import {AuthenticatedUser, AuthService} from '../../services/authentication.service';
-import {Observable} from 'rxjs';
-import {BsModalRef, BsModalService} from 'ngx-foundation';
-import {ModalFileBrowserComponent} from '../modal-file-browser/modal-file-browser.component';
-import {RemoteFile} from 'ng-tapis';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -19,7 +13,7 @@ export class MainComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService, private bsModalService: BsModalService,) {}
+    private authService: AuthService) {}
 
   ngOnInit() {
     this.authService.currentUser.subscribe(next => this.currentUser = next);
