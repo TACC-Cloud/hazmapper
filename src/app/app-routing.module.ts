406d85a242594becc0b1f4577e092c4918f9040a
Nathan Franklin <nathan.franklin@gmail.com>
2021-04-05 13:24:50 -0500

    Remove tracing of routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
   ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [Activate]
 })
