c9b90a116d19424635b15d57f178f1f64e4f9f14
duckonomy <contact@duckonomy.com>
2021-05-12 20:18:03 -0500

    Remove tile layers in map component.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -120,17 +120,8 @@ export class MapComponent implements OnInit, OnDestroy {
     // Subscribe to active project and features
     this.subscription.add(this.loadFeatures());
 
-    const baseOSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    const satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      // tslint:disable-next-line:max-line-length
-      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    });
-    // default to streetmap view;
-    this.map.addLayer(baseOSM);
-
+    // Publish the mouse location on the mapMouseLocation stream
+    this.map.on('mousemove', (ev: LeafletMouseEvent) => this.mouseEventHandler(ev));
 
     // Filter out and display only the active overlays
     this.subscription.add(this.geoDataService.selectedOverlays$
