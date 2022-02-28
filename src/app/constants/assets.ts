9c1dc755d123c60a6a0289d405e60f59e25e3922
Ian Park <contact@duckonomy.com>
2022-02-28 15:01:20 -0600

    Account for no_asset_vectors.

--- src/app/constants/assets.ts
+++ src/app/constants/assets.ts
@@ -1,9 +1,10 @@
-export const featureTypes = ['image', 'video', 'point_cloud'] as const;
+export const featureTypes = ['image', 'video', 'point_cloud', 'no_asset_vector'] as const;
 
 type featuresTypeID = typeof featureTypes[number];
 
 export const featureTypeLabels: Record<featuresTypeID, string> = {
   image: 'Images',
   video: 'Videos',
-  point_cloud: 'Point Clouds'
+  point_cloud: 'Point Clouds',
+  no_asset_vector: 'No Asset Vector'
 } as const;
