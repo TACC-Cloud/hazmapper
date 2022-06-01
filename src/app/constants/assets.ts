24e7697d8bcde8152b76f0c6dbf8965f1e3cb7f1
Ian Park <contact@duckonomy.com>
2022-06-01 15:15:53 -0500

    Add streetview asset type

--- src/app/constants/assets.ts
+++ src/app/constants/assets.ts
@@ -1,4 +1,4 @@
-export const featureTypes = ['image', 'video', 'point_cloud', 'no_asset_vector'] as const;
+export const featureTypes = ['image', 'video', 'point_cloud', 'streetview', 'no_asset_vector'] as const;
 
 type featuresTypeID = typeof featureTypes[number];
 
@@ -6,5 +6,6 @@ export const featureTypeLabels: Record<featuresTypeID, string> = {
   image: 'Images',
   video: 'Videos',
   point_cloud: 'Point Clouds',
+  streetview: 'Streetview',
   no_asset_vector: 'No Asset Vector'
 } as const;
