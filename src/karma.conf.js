e6b6b42d7d4b50748713cee013a667570469c5ee
Ian Park <contact@duckonomy.com>
2022-06-22 17:23:54 -0500

    Add custom launcher for karma.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,8 +25,14 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.DEBUG,
         autoWatch: false,
-        browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadlessCustom'],
         singleRun: false,
         restartOnFileChange: true,
-    });
+        customLaunchers: {
+          ChromeHeadlessCustom: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox', '--disable-gpu']
+          }
+        }
+  });
 };
