273e93314bef7c6e62fa23d77e3aaf423ce1211b
Ian Park <contact@duckonomy.com>
2022-08-19 16:18:03 -0500

    Add basic router.

---
+++ react/src/constants/routes.ts
@@ -0,0 +1,8 @@
+export const MAIN = '/';
+export const LOGIN = '/login';
+export const LOGOUT = '/logout';
+export const PUBLIC_PROJECT = '/project-public/:projectUUID';
+export const PROJECT = '/project/:projectUUID';
+export const CALLBACK = '/callback';
+export const STREETVIEW_CALLBACK = '/streetview/callback';
+export const NOT_FOUND = '/404';
---
+++ react/src/redux/reducers/reducers.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { geoapi } from '../api/geoapi';
+
+export const reducer = combineReducers({
+    [geoapi.reducerPath]: geoapi.reducer,
+});
