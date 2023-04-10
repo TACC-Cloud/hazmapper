f6d8462392e086ca6c0fab4fc48820b442abbd5a
Ian Park <contact@duckonomy.com>
2023-04-11 05:26:52 +0900

    Fix linting.

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv, type UserConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 function getApiUrl(backend: string): string {
@@ -22,7 +22,7 @@ function getPortalUrl(backend: string): string {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
+export default defineConfig(({ command, mode }) => {
   const envFile = loadEnv(mode, process.cwd(), '');
   const env = {
     designSafeUrl: 'https://agave.designsafe-ci.org/',
@@ -37,11 +37,12 @@ export default defineConfig(({command, mode}) => {
     mapillaryTokenUrl: 'https://graph.mapillary.com/token',
     mapillaryApiUrl: 'https://graph.mapillary.com/',
     mapillaryTileUrl: 'https://tiles.mapillary.com/',
-    mapillaryScope: 'user:email+user:read+user:write+public:write+public:upload+private:read+private:write+private:upload',
+    mapillaryScope:
+      'user:email+user:read+user:write+public:write+public:upload+private:read+private:write+private:upload',
     mapillaryClientSecret: '',
     mapillaryClientId: '',
     mapillaryClientToken: '',
-  }
+  };
 
   if (mode === 'production') {
     env.apiUrl = getApiUrl(mode);
@@ -50,38 +51,44 @@ export default defineConfig(({command, mode}) => {
     env.host = 'hazmapper.utexas.edu/hazmapper/';
     env.baseHref = '/hazmapper/';
     env.mapillaryClientId = '5156692464392931';
-    env.mapillaryClientSecret = 'MLY|5156692464392931|6be48c9f4074f4d486e0c42a012b349f';
-    env.mapillaryClientToken = 'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79';
+    env.mapillaryClientSecret =
+      'MLY|5156692464392931|6be48c9f4074f4d486e0c42a012b349f';
+    env.mapillaryClientToken =
+      'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79';
   } else if (mode === 'staging') {
     env.apiUrl = getApiUrl(mode);
     env.portalUrl = getPortalUrl(mode);
     env.clientId = 'foitdqFcimPzKZuMhbQ1oyh3Anka';
     env.host = 'hazmapper.utexas.edu/staging/';
     env.baseHref = '/staging/';
-    env.mapillaryClientSecret = 'MLY|4936281379826603|cafd014ccd8cfc983e47c69c16082c7b';
+    env.mapillaryClientSecret =
+      'MLY|4936281379826603|cafd014ccd8cfc983e47c69c16082c7b';
     env.mapillaryClientId = '4936281379826603';
-    env.mapillaryClientToken = 'MLY|4936281379826603|f8c4732d3c9d96582b86158feb1c1a7a';
+    env.mapillaryClientToken =
+      'MLY|4936281379826603|f8c4732d3c9d96582b86158feb1c1a7a';
   } else {
     env.apiUrl = getApiUrl(envFile.BACKEND);
     env.portalUrl = getPortalUrl(envFile.BACKEND);
     env.clientId = 'Eb9NCCtWkZ83c01UbIAITFvhD9ka';
     env.host = 'hazmapper.local';
     env.baseHref = '/';
-    env.mapillaryClientSecret = 'MLY|5156692464392931|6be48c9f4074f4d486e0c42a012b349f';
+    env.mapillaryClientSecret =
+      'MLY|5156692464392931|6be48c9f4074f4d486e0c42a012b349f';
     env.mapillaryClientId = '5156692464392931';
-    env.mapillaryClientToken = 'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79';
+    env.mapillaryClientToken =
+      'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79';
     env.jwt = envFile.BACKEND === 'development' && envFile.JWT;
-  };
+  }
 
   return {
     plugins: [react()],
     server: {
       port: 4200,
       base: env.baseHref,
-      host: env.host
+      host: env.host,
     },
     define: {
       __ENV__: env,
     },
-  }
+  };
 });
