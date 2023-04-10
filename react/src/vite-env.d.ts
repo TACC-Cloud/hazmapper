e696e5b780f5c04b66162a3f198944828f78440b
Ian Park <contact@duckonomy.com>
2023-04-11 04:17:21 +0900

    Add environments with vite.

--- react/src/vite-env.d.ts
+++ react/src/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+interface Env {
+  designSafeUrl: string,
+  backend: string,
+  apiUrl: string,
+  portalUrl: string,
+  clientId: string,
+  host: string,
+  baseHref: string,
+  jwt: string,
+  mapillaryAuthUrl: string,
+  mapillaryTokenUrl: string,
+  mapillaryApiUrl: string,
+  mapillaryTileUrl: string,
+  mapillaryScope: string,
+  mapillaryClientSecret: string,
+  mapillaryClientId: string,
+  mapillaryClientToken: string,
+}
+
+declare const __ENV__: AppEnv;
--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,11 +1,87 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+function getApiUrl(backend: string): string {
+  if (backend === 'development') {
+    return 'http://localhost:8888';
+  } else if (backend === 'staging') {
+    return 'https://agave.designsafe-ci.org/geo-staging/v2';
+  } else if (backend === 'production') {
+    return 'https://agave.designsafe-ci.org/geo/v2';
+  } else {
+    throw new Error('Unsupported Type');
+  }
+}
+
+function getPortalUrl(backend: string): string {
+  if (backend === 'production') {
+    return 'https://www.designsafe-ci.org/';
+  } else {
+    return 'https://designsafeci-dev.tacc.utexas.edu/';
+  }
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4200,
-    // host: 'hazmapper.local'
-  },
+export default defineConfig(({command, mode}) => {
+  const envFile = loadEnv(mode, process.cwd(), '');
+  const env = {
+    designSafeUrl: 'https://agave.designsafe-ci.org/',
+    backend: envFile.BACKEND,
+    apiUrl: '',
+    portalUrl: '',
+    clientId: '',
+    host: '',
+    baseHref: '',
+    jwt: '',
+    mapillaryAuthUrl: 'https://www.mapillary.com/connect',
+    mapillaryTokenUrl: 'https://graph.mapillary.com/token',
+    mapillaryApiUrl: 'https://graph.mapillary.com/',
+    mapillaryTileUrl: 'https://tiles.mapillary.com/',
+    mapillaryScope: 'user:email+user:read+user:write+public:write+public:upload+private:read+private:write+private:upload',
+    mapillaryClientSecret: '',
+    mapillaryClientId: '',
+    mapillaryClientToken: '',
+  }
+
+  if (mode === 'production') {
+    env.apiUrl = getApiUrl(mode);
+    env.portalUrl = getPortalUrl(mode);
+    env.clientId = 'tMvAiRdcsZ52S_89lCkO4x3d6VMa';
+    env.host = 'hazmapper.utexas.edu/hazmapper/';
+    env.baseHref = '/hazmapper/';
+    env.mapillaryClientId = '5156692464392931';
+    env.mapillaryClientSecret = 'MLY|5156692464392931|6be48c9f4074f4d486e0c42a012b349f';
+    env.mapillaryClientToken = 'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79';
+  } else if (mode === 'staging') {
+    env.apiUrl = getApiUrl(mode);
+    env.portalUrl = getPortalUrl(mode);
+    env.clientId = 'foitdqFcimPzKZuMhbQ1oyh3Anka';
+    env.host = 'hazmapper.utexas.edu/staging/';
+    env.baseHref = '/staging/';
+    env.mapillaryClientSecret = 'MLY|4936281379826603|cafd014ccd8cfc983e47c69c16082c7b';
+    env.mapillaryClientId = '4936281379826603';
+    env.mapillaryClientToken = 'MLY|4936281379826603|f8c4732d3c9d96582b86158feb1c1a7a';
+  } else {
+    env.apiUrl = getApiUrl(envFile.BACKEND);
+    env.portalUrl = getPortalUrl(envFile.BACKEND);
+    env.clientId = 'Eb9NCCtWkZ83c01UbIAITFvhD9ka';
+    env.host = 'hazmapper.local';
+    env.baseHref = '/';
+    env.mapillaryClientSecret = 'MLY|5156692464392931|6be48c9f4074f4d486e0c42a012b349f';
+    env.mapillaryClientId = '5156692464392931';
+    env.mapillaryClientToken = 'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79';
+    env.jwt = envFile.BACKEND === 'development' && envFile.JWT;
+  };
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 4200,
+      base: env.baseHref,
+      host: env.host
+    },
+    define: {
+      __ENV__: env,
+    },
+  }
 });
