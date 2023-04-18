ddd08e37fb62539884efdc20311953659b1bb79b
Ian Park <contact@duckonomy.com>
2023-04-19 05:38:47 +0900

    Add 403 handle.

--- angular/src/app/app.interceptors.ts
+++ angular/src/app/app.interceptors.ts
@@ -77,7 +77,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err) => {
-        if (err.status === 401) {
+        if (err.status === 401 || err.status === 403) {
           // auto logout if 401 response returned from api
           // https://jira.tacc.utexas.edu/browse/DES-1999
           this.authService.logout();
