e833b2481a826b20fb5a6e5e64e2de2552f6dd8a
Nathan Franklin <nathan.franklin@gmail.com>
2023-04-25 22:55:42 -0500

    Fix linting

--- react/src/types/index.ts
+++ react/src/types/index.ts
@@ -1,2 +1,7 @@
 export type { TileServerLayer } from './tileServerLayer';
-export type { Asset, Feature, FeatureClass, FeatureCollection } from './feature';
+export type {
+  Asset,
+  Feature,
+  FeatureClass,
+  FeatureCollection,
+} from './feature';
