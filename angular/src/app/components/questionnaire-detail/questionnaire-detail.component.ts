df2f5ad4fdf7f040346bdadc35f0e431a3096213
Nathan Franklin <nathan.franklin@gmail.com>
2023-12-01 13:11:08 -0600

    task/WG-188: fix questionnaire detail panel (#177)
    * Fix questionnaire-detail so it updates when feature changes
    
    Also handle the cae where there are no properites._hazmapper.questionnaire.asset
    because it is a questionnaire without assets
    
    * Handle case when no feature is active
    
    * Fix linting

--- angular/src/app/components/questionnaire-detail/questionnaire-detail.component.ts
+++ angular/src/app/components/questionnaire-detail/questionnaire-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, SimpleChanges, OnInit, OnChanges } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Feature } from '../../models/models';
 import { QuestionnaireAsset } from '../../models/questionnaire';
@@ -9,7 +9,7 @@ import { QuestionnaireAsset } from '../../models/questionnaire';
   styleUrls: ['./questionnaire-detail.component.styl'],
   providers: [],
 })
-export class QuestionnaireDetailComponent implements OnInit {
+export class QuestionnaireDetailComponent implements OnInit, OnChanges {
   @Input() feature: Feature;
   @Input() featureSource: string;
   assetImages: Array<QuestionnaireAsset>;
@@ -26,24 +26,41 @@ export class QuestionnaireDetailComponent implements OnInit {
     center: false,
   };
 
-  constructor() {}
-
   ngOnInit() {
-    // Retrieve asset images and map them to a new array with proper paths to full/preview images
-    this.assetImages = this.feature.properties._hazmapper.questionnaire.assets.map((asset) => {
-      const pathToFullImage = this.featureSource + '/' + asset.filename;
-      const fileExtension = pathToFullImage.substring(pathToFullImage.lastIndexOf('.'));
-      const pathWithoutExtension = pathToFullImage.substring(0, pathToFullImage.lastIndexOf('.'));
-      const pathToPreviewImage = `${pathWithoutExtension}.preview${fileExtension}`;
+    this.processAssetImages();
+  }
 
-      const filename = asset.filename.split('.');
-      filename.splice(filename.length - 1, 0, 'preview');
-      return {
-        filename: asset.filename,
-        coordinates: asset.coordinates,
-        path: pathToFullImage,
-        previewPath: pathToPreviewImage,
-      };
-    });
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.feature || changes.featureSource) {
+      this.processAssetImages();
+    }
+  }
+
+  private processAssetImages() {
+    if (
+      this.feature.properties &&
+      this.feature.properties._hazmapper &&
+      this.feature.properties._hazmapper.questionnaire &&
+      this.feature.properties._hazmapper.questionnaire.assets
+    ) {
+      // Retrieve asset images and map them to a new array with proper paths to full/preview images
+      this.assetImages = this.feature.properties._hazmapper.questionnaire.assets.map((asset) => {
+        const pathToFullImage = this.featureSource + '/' + asset.filename;
+        const fileExtension = pathToFullImage.substring(pathToFullImage.lastIndexOf('.'));
+        const pathWithoutExtension = pathToFullImage.substring(0, pathToFullImage.lastIndexOf('.'));
+        const pathToPreviewImage = `${pathWithoutExtension}.preview${fileExtension}`;
+
+        const filename = asset.filename.split('.');
+        filename.splice(filename.length - 1, 0, 'preview');
+        return {
+          filename: asset.filename,
+          coordinates: asset.coordinates,
+          path: pathToFullImage,
+          previewPath: pathToPreviewImage,
+        };
+      });
+    } else {
+      this.assetImages = null;
+    }
   }
 }
