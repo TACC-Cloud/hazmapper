fbe4f815eca77a6568cbb36633a39a68d5b446fe
Ian Park <contact@duckonomy.com>
2023-12-16 02:12:28 +0900

    Task/WG-10: Add projects and user queries. (#137)
    * Add login/logout and protected routes
    
    * Refactor
    
    * Fix tests
    
    * Fix linting
    
    * Fix linting
    
    * Add projects and user queries.
    
    * Fix bug.
    
    * Fix test.
    
    * Add prettier.
    
    * Update authUtils.ts
    
    * Update react/src/redux/api/geoapi.ts
    
    * Add link to related jira issue
    
    * Fix prettier issue
    
    ---------
    
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>

--- react/src/components/MainMenu/MainMenu.test.tsx
+++ react/src/components/MainMenu/MainMenu.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import MainMenu from './MainMenu';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
 
 test('renders menu', () => {
-  const { getByText } = render(<MainMenu />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <MainMenu />
+    </Provider>
+  );
   expect(getByText(/Main Menu/)).toBeDefined();
 });
--- react/src/redux/store.ts
+++ react/src/redux/store.ts
@@ -1,9 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducers/reducers';
+import { geoapi } from './api/geoapi';
 
 const store = configureStore({
   reducer: reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      geoapi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 export default store;
