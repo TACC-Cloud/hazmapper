7228e72bd156dffbb7d3f456c00f3fde7b844608
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-12-22 10:38:06 -0600

    task/wg-199-react-query-initial-setup (#185)

---
+++ react/src/hooks/projects/useProjects.ts
@@ -0,0 +1,14 @@
+import { UseQueryResult } from 'react-query';
+import { Project } from '../../types';
+import { useGet } from '../../requests';
+
+const useProjects = (): UseQueryResult<Project[]> => {
+  const query = useGet<Project[]>({
+    endpoint: '/projects/',
+    key: ['projects'],
+    baseUrl: 'https://agave.designsafe-ci.org/geo/v2',
+  });
+  return query;
+};
+
+export default useProjects;
