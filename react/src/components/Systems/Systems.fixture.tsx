5d0d5c457749e76e84b04b041bdd37d28b332b77
shayanaijaz <shayanaijaz@gmail.com>
2024-01-19 12:54:41 -0600

    Added tests

---
+++ react/src/components/Systems/Systems.fixture.tsx
@@ -0,0 +1,19 @@
+const systemsFixture = [
+  {
+    id: 'designsafe.storage.default',
+    name: 'Test Designsafe default storage system',
+    type: 'STORAGE',
+  },
+  {
+    id: 'designsafe.storage.community',
+    name: 'Test Designsafe community storage system',
+    type: 'STORAGE',
+  },
+  {
+    id: 'designsafe.storage.published',
+    name: 'Test Designsafe published storage system',
+    type: 'STORAGE',
+  },
+];
+
+export default systemsFixture;
