a34faf4ed60a007f9c641bbe5490b2976b6fc5ac
Nathan Franklin <nathan.franklin@gmail.com>
2024-01-27 19:46:05 -0600

    Add initial layout

--- react/src/pages/MapProject/MapProject.tsx
+++ react/src/pages/MapProject/MapProject.tsx
@@ -3,6 +3,7 @@ import Map from '../../components/Map';
 import { tileServerLayers } from '../../__fixtures__/tileServerLayerFixture';
 import { featureCollection } from '../../__fixtures__/featuresFixture';
 import { useParams } from 'react-router-dom';
+import styles from './MapProject.module.css';
 
 interface Props {
   /**
@@ -22,12 +23,28 @@ const MapProject: React.FC<Props> = ({ isPublic = false }) => {
   console.log(isPublic);
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      {/* TODO for above and project, should we use style-components and/or CSS modules? */}
-      <Map
-        baseLayers={tileServerLayers}
-        featureCollection={featureCollection}
-      />
+    <div className={styles.root}>
+      <div className={styles.navbar}>MapTopNavBar</div>
+      <div className={styles.mapControlBar}>MapTopControlBar</div>
+      <div className={styles.container}>
+        <div className={styles.leftPanel}>
+          {/* NavBarPanel (name?)  
+             - push button - extends panel to show asset list or other custom ui when button is active
+             - some push buttons - might open Modal
+             
+             */}
+          <div>MapLeftBar Entry 1</div>
+          <div>MapLeftBar Entry 2</div>
+          <div>MapLeftBar Entry 3</div>
+          <div>MapLeftBar Entry 4</div>
+        </div>
+        <div className={styles.map}>
+          <Map
+            baseLayers={tileServerLayers}
+            featureCollection={featureCollection}
+          />
+        </div>
+      </div>
     </div>
   );
 };
