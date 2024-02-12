d4c6114f506f1d1ac2833c44c10cd915c958ffce
Taylor Grafft <tgrafft@TACCs-MacBook-Pro.local>
2024-02-12 13:03:49 -0600

    task/WG-246: Using core-wrapper comps for modal layout

--- react/src/components/CreateMapModal/CreateMapModal.tsx
+++ react/src/components/CreateMapModal/CreateMapModal.tsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
-import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  FormGroup,
-  Label,
-  Input,
-} from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Button } from '../../core-components';
 import styles from './CreateMapModal.module.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import useCreateProject from '../../hooks/projects/useCreateProject';
 import useAuthenticatedUser from '../../hooks/user/useAuthenticatedUser';
 import { useNavigate } from 'react-router-dom';
 import { ProjectRequest } from '../../types';
+import {
+  FieldWrapperFormik,
+  FormikInput,
+  FormikTextarea,
+  FormikCheck,
+} from '../../core-wrappers';
 
 type CreateMapModalProps = {
   isOpen: boolean;
@@ -31,15 +29,22 @@ const validationSchema = Yup.object({
       /^[A-Za-z0-9-_]+$/,
       'Only letters, numbers, hyphens, and underscores are allowed'
     )
-    .required(' file name is required'),
+    .required(' File name is required'),
 });
 
-const CreateMapModal = ({ isOpen, toggle }: CreateMapModalProps) => {
+const CreateMapModal = ({
+  isOpen,
+  toggle: parentToggle,
+}: CreateMapModalProps) => {
   const [errorMessage, setErrorMessage] = useState('');
   const { data: userData } = useAuthenticatedUser();
   const { mutate: createProject, isLoading: isCreatingProject } =
     useCreateProject();
   const navigate = useNavigate();
+  const handleToggle = () => {
+    setErrorMessage(''); // Clear the error message
+    parentToggle(); // Call the original toggle function passed as a prop
+  };
 
   const handleCreateProject = (projectData: ProjectRequest) => {
     createProject(projectData, {
@@ -80,8 +85,8 @@ const CreateMapModal = ({ isOpen, toggle }: CreateMapModalProps) => {
     handleCreateProject(projectData);
   };
   return (
-    <Modal isOpen={isOpen} toggle={toggle}>
-      <ModalHeader toggle={toggle}>Create a New Map</ModalHeader>
+    <Modal isOpen={isOpen} toggle={handleToggle}>
+      <ModalHeader toggle={handleToggle}>Create a New Map</ModalHeader>
       <ModalBody>
         <Formik
           initialValues={{
@@ -95,102 +100,65 @@ const CreateMapModal = ({ isOpen, toggle }: CreateMapModalProps) => {
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
         >
-          {/*TODO_REACT: Will change to core-wrapper's FieldWrapperFormik instead. https://tacc-main.atlassian.net/browse/WG-246 */}
-          {({ errors, touched, values, handleChange }) => (
+          {() => (
             <Form>
-              <FormGroup>
-                <Label for="name">Name</Label>
-                <Field
+              <FieldWrapperFormik name="map-form-info" label="">
+                <FormikInput
                   name="name"
-                  id="name"
-                  className="col-sm-11"
-                  data-testid="name-input"
-                  as={Input}
-                  invalid={touched.name && !!errors.name}
+                  label="Name"
+                  required
+                  className={`${styles['input-height']}`}
                 />
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="description">Description</Label>
-                <Field
+                <FormikTextarea
                   name="description"
-                  id="description"
-                  className="col-sm-11"
-                  as={Input}
-                  invalid={touched.description && !!errors.description}
+                  label="Description"
+                  required
+                  className={`${styles['input-height']}`}
                 />
-                <ErrorMessage
-                  name="description"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="system_file">Custom File Name</Label>
-                <div className="input-group">
-                  <Field
-                    name="system_file"
-                    id="system_file"
-                    as={Input}
-                    className="col-sm-8"
-                    invalid={touched.system_file && !!errors.system_file}
-                  />
-                  <span className="input-group-text col-sm-3">.hazmapper</span>
-                </div>
-                {/* Alt solution to render error message bc input group was causing text to not display properly */}
-                {touched.system_file && errors.system_file && (
-                  <div className={`${styles['custom-error-message']}`}>
-                    {errors.system_file}
-                  </div>
-                )}
-              </FormGroup>
-              {/* TODO_REACT: This part will change once the FileBrowser component is added. https://tacc-main.atlassian.net/browse/WG-208*/}
-              <FormGroup className="row align-items-center">
-                <Label className="col-sm-4">Save Location:</Label>
-                <div className="col-sm-8 text-primary">
-                  {userData ? `/${userData.username}` : 'Loading...'}
-                </div>
-              </FormGroup>
-              <FormGroup className="row mb-2">
-                <Label className="col-sm-4 col-form-label pt-0">
-                  Sync Folder:
-                </Label>
-                <div className="col-sm-8">
-                  <div className="form-check">
-                    <Field
-                      type="checkbox"
-                      name="syncFolder"
-                      id="syncFolder"
-                      className="form-check-input"
-                      checked={values.syncFolder}
-                      onChange={handleChange}
+                <div className={`${styles['flexContainer']}`}>
+                  <span>
+                    <FormikInput
+                      name="system_file"
+                      label="Custom File Name"
+                      required
+                      className={`${styles['input-custom-size']}`}
                     />
-                  </div>
-                  <br />
-                  <br />
-                  <Label
-                    className={`${styles['form-check-label']}`}
-                    for="syncFolder"
-                  >
-                    When enabled, files in this folder are automatically synced
-                    into the map periodically.
-                  </Label>
+                  </span>
+                  <span className={`${styles['hazmapper-custom']}`}>
+                    .hazmapper
+                  </span>
                 </div>
-              </FormGroup>
+                <div className={`${styles['flexContainer']}`}>
+                  <label htmlFor="save-location-label">Save Location:</label>
+                  <span className="text-primary">/{userData?.username}</span>
+                </div>
+                <div className={`${styles['flexContainer']}`}>
+                  <label htmlFor="sync-folder-label">Sync Folder:</label>
+                  <span className={`${styles['check-wrapper']}`}>
+                    <FormikCheck name="syncFolder" label="" description="" />
+                  </span>
+                </div>
+                <div className={`${styles['custom-sync-description']}`}>
+                  When enabled, files in this folder are automatically synced
+                  into the map periodically.
+                </div>
+              </FieldWrapperFormik>
               {errorMessage && (
                 <div className={`${styles['custom-error-message']}`}>
                   {errorMessage}
                 </div>
               )}
               <ModalFooter className="justify-content-start">
-                <Button size="short" type="secondary" onClick={toggle}>
+                <Button
+                  className="justify-content-center"
+                  size="short"
+                  type="secondary"
+                  onClick={handleToggle}
+                >
                   Close
                 </Button>
                 <Button
+                  className="justify-content-center"
                   size="short"
                   type="primary"
                   attr="submit"
