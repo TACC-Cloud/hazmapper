1ab5bc06212978bb836c4225393e648b2c0f9d27
Nathan Franklin <nathan.franklin@gmail.com>
2024-02-14 16:57:29 -0600

    Use asset images for navbar

--- react/src/components/MapProjectNavBar/MapProjectNavBar.tsx
+++ react/src/components/MapProjectNavBar/MapProjectNavBar.tsx
@@ -4,92 +4,92 @@ import styles from './MapProjectNavBar.module.css';
 import { QueryNavItem } from '../../core-wrappers';
 import { queryPanelKey, Panel } from '../../utils/panels';
 
+import assetsImage from '../../assets/assets.png';
+import pointCloudImage from '../../assets/point-clouds.png';
+import layersImage from '../../assets/layers.png';
+import filtersImage from '../../assets/filters.png';
+import streetviewImage from '../../assets/streetview.png';
+import manageImage from '../../assets/users-solid.png';
+
 interface NavItem {
   label: string;
-  icon_image: string;
+  imagePath: string;
   panel: Panel;
   showWhenPublic: boolean;
 }
 
+const navItems: NavItem[] = [
+  {
+    label: 'Assets',
+    imagePath: assetsImage,
+    panel: Panel.Assets,
+    showWhenPublic: true,
+  },
+  {
+    label: 'Point Clouds',
+    imagePath: pointCloudImage,
+    panel: Panel.PointClouds,
+    showWhenPublic: false,
+  },
+  {
+    label: 'Layers',
+    imagePath: layersImage,
+    panel: Panel.Layers,
+    showWhenPublic: true,
+  },
+  {
+    label: 'Filters',
+    imagePath: filtersImage,
+    panel: Panel.Filters,
+    showWhenPublic: true,
+  },
+  {
+    label: 'Streetview',
+    imagePath: streetviewImage,
+    panel: Panel.Streetview,
+    showWhenPublic: true,
+  },
+  {
+    label: 'Manage',
+    imagePath: manageImage,
+    panel: Panel.Manage,
+    showWhenPublic: true,
+  },
+];
+
 interface NavBarPanelProps {
-  items?: NavItem[];
   isPublic?: boolean;
 }
 
-const MapProjectNavBar: React.FC<NavBarPanelProps> = ({
-  items = [],
-  isPublic = false,
-}) => {
+const MapProjectNavBar: React.FC<NavBarPanelProps> = ({ isPublic = false }) => {
   const location = useLocation();
-
   const queryParams = new URLSearchParams(location.search);
   const activePanel = queryParams.get(queryPanelKey);
 
-  console.log(items);
-  console.log(isPublic);
-
   return (
     <div className={styles.root}>
-      <QueryNavItem
-        icon="applications" /* TODO_REACT*/
-        to={
-          activePanel == Panel.Assets ? '' : `?${queryPanelKey}=${Panel.Assets}`
-        }
-        active={activePanel == Panel.Assets}
-      >
-        Assets
-      </QueryNavItem>
-      <QueryNavItem
-        icon="upload" /* TODO_REACT*/
-        to={
-          activePanel == Panel.PointClouds
-            ? ''
-            : `?${queryPanelKey}=${Panel.PointClouds}`
-        }
-        active={activePanel == Panel.PointClouds}
-      >
-        Point Clouds
-      </QueryNavItem>
-      <QueryNavItem
-        icon="burger" /* TODO_REACT*/
-        to={
-          activePanel == Panel.Layers ? '' : `?${queryPanelKey}=${Panel.Layers}`
-        }
-        active={activePanel == Panel.Layers}
-      >
-        Layers
-      </QueryNavItem>
-      <QueryNavItem
-        icon="search" /* TODO_REACT*/
-        to={
-          activePanel == Panel.Filters
-            ? ''
-            : `?${queryPanelKey}=${Panel.Filters}`
-        }
-        active={activePanel == Panel.Filters}
-      >
-        Filters
-      </QueryNavItem>
-      <QueryNavItem
-        icon="reverse-order" /* TODO_REACT*/
-        to={
-          activePanel == Panel.Streetview
-            ? ''
-            : `?${queryPanelKey}=${Panel.Streetview}`
-        }
-        active={activePanel == Panel.Streetview}
-      >
-        Streetview
-      </QueryNavItem>
-      <QueryNavItem
-        icon="project" /* TODO_REACT*/
-        to={
-          activePanel == Panel.Manage ? '' : `?${queryPanelKey}=${Panel.Manage}`
-        }
-        active={activePanel == Panel.Manage}
-      >
-        Manage
-      </QueryNavItem>
+      {navItems
+        .filter((item) => (isPublic ? item.showWhenPublic : true))
+        .map((item) => (
+          <QueryNavItem
+            key={item.panel}
+            to={
+              activePanel === item.panel
+                ? ''
+                : `?${queryPanelKey}=${item.panel}`
+            }
+            active={activePanel === item.panel}
+          >
+            <img
+              src={item.imagePath}
+              alt={item.label}
+              className={styles.image}
+              width="32px"
+            />
+            {/*TODO_REACT do we want to bold the text if active like in CEP-portals?*/}
+            {item.label}
+          </QueryNavItem>
+        ))}
     </div>
   );
 };
