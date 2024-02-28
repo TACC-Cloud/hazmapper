41a026c2de7c0b3ec9e44aa05107de3ceed05b20
Taylor Grafft <tjgrafft@me.com>
2024-02-28 15:08:37 -0600

    task/WG-246: new CreateMapModal layout w/ core-wrappers (#208)
    * task/WG-74: Create Map React Modal
    
    * recreating lock file
    
    * working POST request and unit tests added
    
    * made system_path dynamic to current user that's logged in
    
    * fixing unit tests and modifying auth.ts file for AuthenticatedUser
    
    * adding tacc-core-styles to Modal
    
    * fixing lint issues and unit test
    
    * refactoring usePost and useCreateProject to be more efficient
    
    * renaming MapModal comp and adding error message to modal
    
    * adding useAuthenticatedUser hook and replacing redux
    
    * Refactoring CreateMapModal to handle its own states and errors instead of MainMenu
    
    * fixing MainMenu comp to match master
    
    * Updating comments and type cleanup
    
    * removing actions and submitting state management
    
    * Modifying error message handling
    
    * Updating CreateMapModal test to reflect new error handling
    
    * updating package.json to fix conflicts with lock file
    
    * task/WG-246: Using core-wrapper comps for modal layout
    
    * updating config and unit tests for new wrapper components
    
    * removing Wizard wrapper comp and updating modal layout
    
    * fixing lint for css file
    
    * removing justify-content-center from buttons and upgrading tacc-core-styles version
    
    * fixing formatting and cssminifying side effects from core-styles upgrade
    
    * adding UI suggestions
    
    * adding alt Save Location UI
    
    * Refactoring FormikCheck's styling/layout
    
    * removing unused css rules from CreateMapModal module css file
    
    * fixing horizontal scroll and adding hazmapper suffix and mobile friendly
    
    ---------
    
    Co-authored-by: Taylor Grafft <tgrafft@TACCs-MBP.attlocal.net>
    Co-authored-by: Taylor Grafft <tgrafft@wireless-10-147-86-245.public.utexas.edu>
    Co-authored-by: Taylor Grafft <tgrafft@wireless-10-147-45-228.public.utexas.edu>
    Co-authored-by: Taylor Grafft <tgrafft@TACCs-MacBook-Pro.local>
    Co-authored-by: Taylor Grafft <tgrafft@wireless-10-150-14-226.public.utexas.edu>
    Co-authored-by: Taylor Grafft <tgrafft@wireless-10-150-206-183.public.utexas.edu>
    Co-authored-by: Taylor Grafft <tgrafft@wireless-10-150-200-98.public.utexas.edu>
    Co-authored-by: Taylor Grafft <tgrafft@wireless-10-150-180-218.public.utexas.edu>

--- react/src/core-wrappers/index.ts
+++ react/src/core-wrappers/index.ts
@@ -1,9 +1,6 @@
-import { WizardStep as WizardStepType } from './Wizard';
 export { Navbar, NavItem, QueryNavItem, AnchorNavItem } from './Navbar';
 export { default as QueryWrapper } from './QueryWrapper';
 export { default as SubmitWrapper } from './SubmitWrapper';
-export { default as Wizard, useWizard, WizardNavigation } from './Wizard';
-export type WizardStep<T> = WizardStepType<T>;
 export {
   FieldWrapperFormik,
   FormikInput,
