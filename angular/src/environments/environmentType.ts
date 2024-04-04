d71f358f27688d52515a67bfdc313738ae93aeed
Nathan Franklin <nathan.franklin@gmail.com>
2024-04-04 10:50:00 -0500

    Add experimental backend adn new tapis client ids

--- angular/src/environments/environmentType.ts
+++ angular/src/environments/environmentType.ts
@@ -2,5 +2,6 @@ export enum EnvironmentType {
   Production = 'production',
   Staging = 'staging',
   Dev = 'dev' /* i.e. dev.geoapi-services.tacc.utexas.edu*/,
+  Experimental = 'experimental' /* i.e. experimental.geoapi-services.tacc.utexas.edu*/,
   Local = 'local',
 }
