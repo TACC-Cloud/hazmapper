9fb41e7384fd081f4e3de4006fbd07aa61f8c408
Taylor Grafft <tgrafft@TACCs-MacBook-Pro.local>
2024-04-08 11:44:59 -0500

    refactoring Create Map button to be inside ProjectListings. Adding trigger buttons for notification examples

--- react/src/pages/MainMenu/MainMenu.tsx
+++ react/src/pages/MainMenu/MainMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
   LoadingSpinner,
@@ -10,11 +10,12 @@ import {
 } from '../../core-components';
 import useAuthenticatedUser from '../../hooks/user/useAuthenticatedUser';
 import { SystemSelect } from '../../components/Systems';
-import CreateMapModal from '../../components/CreateMapModal/CreateMapModal';
 import { ProjectListing } from '../../components/Projects/ProjectListing';
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:8888');
+import {
+  setupSocketListeners,
+  removeSocketListeners,
+  socket,
+} from '../../utils/socketUtils';
 
 function MainMenu() {
   const {
@@ -22,41 +23,24 @@ function MainMenu() {
     isLoading: isUserLoading,
     error: userError,
   } = useAuthenticatedUser();
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [connectionStatus, setConnectionStatus] = useState('Disconnected');
 
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
+  const triggerNotification = () => {
+    socket.emit('trigger_notification', {
+      message: 'Hello from the client!',
+    });
   };
 
   const [selectedSystem, setSelectedSystem] = useState('');
 
   useEffect(() => {
-    // Listen for the 'connect' and 'disconnect' events to update the connection status
-    socket.on('connect', () => {
-      setConnectionStatus('Connected');
-    });
-
-    socket.on('disconnect', () => {
-      setConnectionStatus('Disconnected');
-    });
-
-    socket.on('connect_error', (error) => {
-      console.error('Connection Error:', error);
-    });
-
-    socket.on('notifications', (data) => {
-      toast.info(data.message);
-    });
+    setupSocketListeners(setConnectionStatus);
 
     // Clean up the event listeners when the component unmounts
     return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('connect_error');
-      socket.off('notifications');
+      removeSocketListeners();
     };
-  }, []);
+  }, [setConnectionStatus]);
 
   if (isUserLoading) {
     return (
@@ -84,14 +68,15 @@ function MainMenu() {
         WebSocket Status: {connectionStatus}
       </InlineMessage>
       <div>
-        <Button type="primary" size="small" onClick={toggleModal}>
-          Create Map
+        <InlineMessage type="info">
+          Welcome, {userData?.username || 'User'} <Icon name="user"></Icon>
+        </InlineMessage>
+      </div>
+      <div>
+        <Button type="primary" onClick={triggerNotification}>
+          Trigger Notification
         </Button>
       </div>
-      <InlineMessage type="info">
-        Welcome, {userData?.username || 'User'} <Icon name="user"></Icon>
-      </InlineMessage>
-      <CreateMapModal isOpen={isModalOpen} toggle={toggleModal} />
       <ProjectListing />
       <ToastContainer />
       {selectedSystem && <div>Current system selected: {selectedSystem}</div>}
---
+++ react/src/utils/socketUtils.ts
@@ -0,0 +1,45 @@
+import io from 'socket.io-client';
+import { toast } from 'react-toastify';
+
+export const socket = io('http://localhost:8888');
+
+export const setupSocketListeners = (updateConnectionStatus) => {
+  socket.on('connect', () => {
+    updateConnectionStatus('Connected');
+    console.log('Connected');
+  });
+
+  socket.on('disconnect', () => {
+    updateConnectionStatus('Disconnected');
+    console.log('Disconnected');
+  });
+
+  socket.on('connect_error', (error) => {
+    console.error('Connection Error:', error);
+  });
+
+  socket.on('notification', (data) => {
+    console.log('Notification received:', data.message);
+    toast.info(data.message);
+  });
+
+  socket.on('asset_success', (data) => {
+    console.log('Notification received:', data.message);
+    toast.success(data.message);
+  });
+
+  socket.on('asset_failure', (data) => {
+    console.log('Notification received:', data.message);
+    toast.error(data.message);
+  });
+};
+
+// Clean up function to remove all listeners
+export const removeSocketListeners = () => {
+  socket.off('connect');
+  socket.off('disconnect');
+  socket.off('connect_error');
+  socket.off('notification');
+  socket.off('asset_success');
+  socket.off('asset_failure');
+};
