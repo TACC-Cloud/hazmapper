d9cb450c5dc1e38fc3cce7c851252b00bd493fba
Nathan Franklin <nathan.franklin@gmail.com>
2024-07-25 09:45:59 -0500

    Fix prod env

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
-import { EnvironmentType } from './environmentType';
+import { DesignSafeEnvironmentType, EnvironmentType } from './environmentType';
 
 export interface AppEnvironment {
   jwt?: string;
   production: boolean;
   backend: EnvironmentType;
+  designSafePortal: DesignSafeEnvironmentType;
 }
 
 export const environment: AppEnvironment = {
   production: true,
   // only used during testing
   backend: EnvironmentType.Production,
+  designSafePortal: DesignSafeEnvironmentType.Production
 };
