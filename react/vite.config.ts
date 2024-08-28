4afc9d85d765af9a87e8b5e70e40a60d23c19438
Nathan Franklin <nathan.franklin@gmail.com>
2024-08-28 13:53:50 -0500

    Add visualizer

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line
 export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      visualizer({
+        open: false,
+        filename: 'dist/stats.html',
+        gzipSize: true,
+        brotliSize: true,
+      }),
+    ],
     server: {
       port: 4200,
       host: 'localhost',
