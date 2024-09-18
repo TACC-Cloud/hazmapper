055120a035aa7cc4ab4cf14e23663e7d9a09b2a9
jgentle <jgentle@tacc.utexas.edu>
2024-09-18 12:33:14 -0500

    Rough content layout for mngmt panel.

--- react/src/components/ManageMapProjectModal/ManageMapProjectModal.tsx
+++ react/src/components/ManageMapProjectModal/ManageMapProjectModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { Button, Container, Row, Col, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import styles from './ManageMapProjectModal.module.css';
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 interface ManageMapProjectModalProps {
   isPublic: boolean;
@@ -20,18 +20,50 @@ const ManageMapProjectModal: React.FC<ManageMapProjectModalProps> = ({
   };
 
   return (
-    <Modal isOpen toggle={closeModal}>
+    <Modal classNme="modal-dialog-centered" size="lg" isOpen toggle={closeModal}>
       <ModalHeader toggle={closeModal}>Manage Map</ModalHeader>
       <ModalBody>
-        <div className={styles.root}>
-          <h3 style={{ marginBottom: '10px' }}>Map Details</h3>
-          <h4 style={{ marginBottom: '10px' }}>Map Name: TBD</h4>
-          <h4 style={{ marginBottom: '10px' }}>Map Description: TBD</h4>
-          <h4 style={{ marginBottom: '10px' }}>Map Saved Location: TBD</h4>
-          <h4 style={{ marginBottom: '10px' }}>Map Is-Public: {isPublic}</h4>
-          <h3 style={{ marginBottom: '10px' }}>Map Users</h3>
-          <h4 style={{ marginBottom: '10px' }}>Current Members: TBD</h4>
-        </div>
+        {/* <div className={styles.root}></div> */}
+        <Container>
+          <Row>
+            <Col lg="8">
+            <div>
+              <h3 style={{ marginBottom: '10px' }}>Map Details</h3>
+              <h4 style={{ marginBottom: '10px' }}>Map Name: TBD</h4>
+              <h4 style={{ marginBottom: '10px' }}>Map Description: TBD</h4>
+              <h4 style={{ marginBottom: '10px' }}>Map Saved Location: TBD</h4>
+              <h4 style={{ marginBottom: '10px' }}>Map Is-Public: {isPublic}</h4>
+              <Button
+                style={{ marginBottom: '10px' }}
+                // className={}
+                type="secondary"
+                // iconNameBefore="trash"
+                // attr="submit"
+                // isLoading={isFetching}
+              >
+                Make Map Public
+              </Button>
+              <br/>
+              <Button
+              style={{ marginTop: '10px' }}
+                // className={}
+                type="secondary"
+                // iconNameBefore="trash"
+                // attr="submit"
+                // isLoading={isFetching}
+              >
+                Delete Map
+              </Button>
+            </div>
+            </Col>
+            <Col lg="4">
+            <div>
+              <h3 style={{ marginBottom: '10px' }}>Map Users</h3>
+              <h4 style={{ marginBottom: '10px' }}>Current Members: TBD</h4>  
+            </div>
+            </Col>
+          </Row>
+        </Container>
       </ModalBody>
     </Modal>
   );
