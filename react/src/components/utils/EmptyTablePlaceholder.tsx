d55959d03362f081f0817135baf579ea01beadc1
sophia-massie <smassie@tacc.utexas.edu>
2024-10-03 18:28:12 -0500

    - Linting

--- react/src/components/utils/EmptyTablePlaceholder.tsx
+++ react/src/components/utils/EmptyTablePlaceholder.tsx
@@ -2,21 +2,22 @@ import { SectionMessage } from '../../core-components';
 import styles from './EmptyTablePlaceholder.module.css';
 import React from 'react';
 
-
 interface EmptyPlaceholderProps {
   children?: React.ReactNode;
-  type: 'error' | 'warning' | 'info' | 'success'; 
+  type: 'error' | 'warning' | 'info' | 'success';
 }
 
 export const EmptyTablePlaceholder: React.FC<EmptyPlaceholderProps> = ({
   children,
-  type, 
+  type,
 }) => {
   return (
     <div className={styles['empty']}>
       {children ? (
         <SectionMessage type={type}>{children}</SectionMessage>
-      ) : <SectionMessage type={type}>No data available.</SectionMessage>}
+      ) : (
+        <SectionMessage type={type}>No data available.</SectionMessage>
+      )}
     </div>
   );
 };
