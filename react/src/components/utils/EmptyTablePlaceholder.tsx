0ff0f14ecf7356ce9af257b832aae0a7886b2e2f
sophia-massie <smassie@tacc.utexas.edu>
2024-10-03 18:25:25 -0500

    - Addresses Type 'ReactNode' is not assignable to type 'NonNullable<ReactNodeLike>' error in unit test

--- react/src/components/utils/EmptyTablePlaceholder.tsx
+++ react/src/components/utils/EmptyTablePlaceholder.tsx
@@ -2,18 +2,21 @@ import { SectionMessage } from '../../core-components';
 import styles from './EmptyTablePlaceholder.module.css';
 import React from 'react';
 
+
 interface EmptyPlaceholderProps {
-  children: React.ReactNode;
-  type: 'error' | 'warning' | 'info' | 'success';
+  children?: React.ReactNode;
+  type: 'error' | 'warning' | 'info' | 'success'; 
 }
 
 export const EmptyTablePlaceholder: React.FC<EmptyPlaceholderProps> = ({
   children,
-  type,
+  type, 
 }) => {
   return (
     <div className={styles['empty']}>
-      <SectionMessage type={type}>{children}</SectionMessage>
+      {children ? (
+        <SectionMessage type={type}>{children}</SectionMessage>
+      ) : <SectionMessage type={type}>No data available.</SectionMessage>}
     </div>
   );
 };
