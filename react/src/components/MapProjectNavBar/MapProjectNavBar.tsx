f3142d9e885db1d0c95816565a52e298c0e43849
Nathan Franklin <nathan.franklin@gmail.com>
2024-10-15 16:29:50 -0500

    Add local version of QueryNavItem to illustrate issue

--- react/src/components/MapProjectNavBar/MapProjectNavBar.tsx
+++ react/src/components/MapProjectNavBar/MapProjectNavBar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from './MapProjectNavBar.module.css';
 import { QueryNavItem } from '@tacc/core-components';
+// TODO:not needed but can help illustate the `error useLocation() in the context of a <Router>` and how its in the tacc/core-components lib
+//import { QueryNavItem } from './QueryNavItem';
 import { queryPanelKey, Panel } from '../../utils/panels';
 
 import assetsImage from '../../assets/assets.png';
