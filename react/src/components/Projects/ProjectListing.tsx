dfa4af2dcf9952091d3b94a5b382533c41ee82e4
sophia-massie <smassie@tacc.utexas.edu>
2024-10-28 17:16:43 -0500

    - Address changes - removed fontawesome, error handling, removed global styles to apply at end

--- react/src/components/Projects/ProjectListing.tsx
+++ react/src/components/Projects/ProjectListing.tsx
@@ -18,89 +18,77 @@ const ProjectListing: React.FC = () => {
     setIsModalOpen(!isModalOpen);
   };
 
-  const { data, isLoading, isError } = useProjectsWithDesignSafeInformation();
+  const { data, isLoading, isError, error } =
+    useProjectsWithDesignSafeInformation();
 
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
+  if (isError) {
+    return (
+      <div className={styles.root}>
+          <EmptyTablePlaceholder type="error">
+            There was an error gathering your maps.{' '}
+            {/* @ts-ignore: Suppress error typing issues */}
+            {error?.message ? error?.message : 'An unknown error occurred.'}
+            <br />
+            <a
+              href="https://www.designsafe-ci.org/help/new-ticket/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Click here to submit a ticket on DesignSafe.
+            </a>
+          </EmptyTablePlaceholder>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.root}>
-      <div className={styles.projectList}>
-        <table>
-          <thead className={styles.projectHeader}>
+      {data && data.length > 0 ? (
+        <table className={styles.projectList} >
+          <thead >
             <tr>
-              <th className={styles.mapColumn}>Map</th>
-              <th className={styles.projectColumn}>Project</th>
+              <th>Map</th>
+              <th>Project</th>
               <th className={styles.buttonColumn}>
                 <CreateMapModal isOpen={isModalOpen} toggle={toggleModal} />
                 <Button
                   onClick={toggleModal}
                   type="link"
-                  className={styles.projectListItemButton}
+                  iconNameBefore="add"
                 >
-                  <Icon name="add" />
                   Create a New Map
                 </Button>
               </th>
             </tr>
           </thead>
           <tbody>
-            {isError && (
-              <tr>
-                <td colSpan={3}>
-                  <EmptyTablePlaceholder type="error">
-                    There was an error gathering your maps and projects. {''}
-                    <a
-                      href="https://www.designsafe-ci.org/help/new-ticket/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Click here to submit a ticket on DesignSafe.
-                    </a>
-                  </EmptyTablePlaceholder>
+            {data?.map((proj) => (
+              <tr key={proj.id} onClick={() => navigateToProject(proj.uuid)}>
+                <td>{proj.name}</td>
+                <td>
+                  {proj.ds_project?.value.projectId}
+                  {' - '}
+                  {proj.ds_project?.value.title}
+                </td>
+                <td className={styles.buttonColumn}>
+                  <Button type="link" iconNameBefore="edit-document"></Button>
+                  <Button type="link" iconNameBefore="trash"></Button>
                 </td>
               </tr>
-            )}
-            {data && data.length > 0 ? (
-              data?.map((proj) => (
-                <tr key={proj.id} onClick={() => navigateToProject(proj.uuid)}>
-                  <td className={styles.projectName}>{proj.name}</td>
-                  <td>
-                    {proj.ds_project?.value.projectId}
-                    {' - '}
-                    {proj.ds_project?.value.title}
-                  </td>
-                  <td>
-                    <Button
-                      type="link"
-                      className={styles.projectListItemButton}
-                    >
-                      <Icon name="edit-document" />
-                    </Button>
-                    <Button
-                      type="link"
-                      className={styles.projectListItemButton}
-                    >
-                      <Icon name="trash" />
-                    </Button>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={3}>
-                  <EmptyTablePlaceholder type="info">
-                    No projects or maps found.
-                    <br />
-                    Click Create New Map above to get started.
-                  </EmptyTablePlaceholder>
-                </td>
-              </tr>
-            )}
+            ))}
           </tbody>
         </table>
-      </div>
+      ) : (
+        <EmptyTablePlaceholder type="info">
+          No maps found.
+          <br />
+          Click Create New Map above to get started.
+        </EmptyTablePlaceholder>
+      )}
     </div>
   );
 };
