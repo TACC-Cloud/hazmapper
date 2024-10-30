aaab52b2c65388407ded75b006d84f2f74592f7f
jgentle <jgentle@tacc.utexas.edu>
2024-10-30 10:56:47 -0500

    Changing to a panel layout instead of modal for Manage Project view.

--- react/src/components/ManageMapProjectModal/ManageMapProjectModal.tsx
+++ react/src/components/ManageMapProjectModal/ManageMapProjectModal.tsx
@@ -4,7 +4,7 @@ import { Button, Container, Row, Col, Modal, ModalHeader, ModalBody } from 'reac
 import styles from './ManageMapProjectModal.module.css';
 
 interface ManageMapProjectModalProps {
-  isPublic: boolean;
+  // isPublic: boolean;
 }
 
 const ManageMapProjectModal: React.FC<ManageMapProjectModalProps> = ({
@@ -20,52 +20,55 @@ const ManageMapProjectModal: React.FC<ManageMapProjectModalProps> = ({
   };
 
   return (
-    <Modal className="modal-dialog-centered" size="lg" isOpen toggle={closeModal}>
-      <ModalHeader toggle={closeModal}><b>Manage Map Project</b></ModalHeader>
-      <ModalBody>
-        <Container>
-          <Row>
-            <Col lg="6">
-              <div className={styles.root}>
-                <h3 style={{ marginBottom: '10px' }}>Map Project Information</h3>
-                <div style={{ marginBottom: '10px' }}>Name: TBD</div>
-                <div style={{ marginBottom: '10px' }}>Description: TBD</div>
-                <div style={{ marginBottom: '10px' }}>Saved Location: TBD</div>
-                <div style={{ marginBottom: '10px' }}>Is-Public: {isPublic}</div>
-              </div>
-            </Col>
-            <Col lg="6">
-              <div className={styles.root}>
-                <h3 style={{ marginBottom: '10px' }}>Map Project Members</h3>
-                <ul>
-                  <li>TBD</li>
-                </ul>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg="6">
-              <Button
-                style={{ width: '12rem', marginBottom: '10px' }}
-                color="success"
-                size="md"
-              >
-                Make Map Public
-              </Button>
-            </Col>
-            <Col lg="6">
-              <Button
-                style={{ width: '12rem', marginBottom: '10px' }}
-                color="danger"
-                size="md"
-              >
-                Delete Map
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-      </ModalBody>
-    </Modal>
+    // <Modal className="modal-dialog-centered" size="lg" isOpen toggle={closeModal}>
+    //   <ModalHeader toggle={closeModal}><b>Manage Map Project</b></ModalHeader>
+    //   <ModalBody>
+    //     <Container>
+    //       <Row>
+    //         <Col lg="6">
+    //           <div className={styles.root}>
+    //             <h3 style={{ marginBottom: '10px' }}>Map Project Information</h3>
+    //             <div style={{ marginBottom: '10px' }}>Name: TBD</div>
+    //             <div style={{ marginBottom: '10px' }}>Description: TBD</div>
+    //             <div style={{ marginBottom: '10px' }}>Saved Location: TBD</div>
+    //             <div style={{ marginBottom: '10px' }}>Is-Public: {isPublic}</div>
+    //           </div>
+    //         </Col>
+    //         <Col lg="6">
+    //           <div className={styles.root}>
+    //             <h3 style={{ marginBottom: '10px' }}>Map Project Members</h3>
+    //             <ul>
+    //               <li>TBD</li>
+    //             </ul>
+    //           </div>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col lg="6">
+    //           <Button
+    //             style={{ width: '12rem', marginBottom: '10px' }}
+    //             color="success"
+    //             size="md"
+    //           >
+    //             Make Map Public
+    //           </Button>
+    //         </Col>
+    //         <Col lg="6">
+    //           <Button
+    //             style={{ width: '12rem', marginBottom: '10px' }}
+    //             color="danger"
+    //             size="md"
+    //           >
+    //             Delete Map
+    //           </Button>
+    //         </Col>
+    //       </Row>
+    //     </Container>
+    //   </ModalBody>
+    // </Modal>
+    <div className={styles.root}>
+      <h1>Map Management Panel</h1>
+    </div>
   );
 };
 
