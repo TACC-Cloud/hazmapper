e5c600bd847544dd4d94d43855cc2fe10a703915
jgentle <jgentle@tacc.utexas.edu>
2024-10-30 15:35:16 -0500

    Fixed CSS selector use in management panel.

--- react/src/components/ManageMapProjectModal/ManageMapProjectModal.tsx
+++ react/src/components/ManageMapProjectModal/ManageMapProjectModal.tsx
@@ -1,75 +1,52 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { Button, Container, Row, Col, Modal, ModalHeader, ModalBody } from 'reactstrap';
-import styles from './ManageMapProjectModal.module.css';
+import styles from './ManageMapProjectPanel.module.css';
+import { Button } from 'reactstrap';
 
-interface ManageMapProjectModalProps {
-  // isPublic: boolean;
+interface ManageMapProjectPanelProps {
+  /**
+   * Whether or not the map project is public.
+   */
+  isPublic: boolean;
 }
 
-const ManageMapProjectModal: React.FC<ManageMapProjectModalProps> = ({
-  // isPublic,
-}) => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const closeModal = () => {
-    const params = new URLSearchParams(location.search);
-    params.delete('panel'); // Remove the panel query parameter
-    navigate(`${location.pathname}?${params.toString()}`); // Update the URL
-  };
-
+/**
+ * A component that displays a panel for managing the map project.
+ */
+const ManageMapProjectPanel: React.FC<ManageMapProjectPanelProps> = ({ isPublic }) => {
+  // Testing panel behavior. Seeing the panel element displace the map as opposed to overlay.
   return (
-    // <Modal className="modal-dialog-centered" size="lg" isOpen toggle={closeModal}>
-    //   <ModalHeader toggle={closeModal}><b>Manage Map Project</b></ModalHeader>
-    //   <ModalBody>
-    //     <Container>
-    //       <Row>
-    //         <Col lg="6">
-    //           <div className={styles.root}>
-    //             <h3 style={{ marginBottom: '10px' }}>Map Project Information</h3>
-    //             <div style={{ marginBottom: '10px' }}>Name: TBD</div>
-    //             <div style={{ marginBottom: '10px' }}>Description: TBD</div>
-    //             <div style={{ marginBottom: '10px' }}>Saved Location: TBD</div>
-    //             <div style={{ marginBottom: '10px' }}>Is-Public: {isPublic}</div>
-    //           </div>
-    //         </Col>
-    //         <Col lg="6">
-    //           <div className={styles.root}>
-    //             <h3 style={{ marginBottom: '10px' }}>Map Project Members</h3>
-    //             <ul>
-    //               <li>TBD</li>
-    //             </ul>
-    //           </div>
-    //         </Col>
-    //       </Row>
-    //       <Row>
-    //         <Col lg="6">
-    //           <Button
-    //             style={{ width: '12rem', marginBottom: '10px' }}
-    //             color="success"
-    //             size="md"
-    //           >
-    //             Make Map Public
-    //           </Button>
-    //         </Col>
-    //         <Col lg="6">
-    //           <Button
-    //             style={{ width: '12rem', marginBottom: '10px' }}
-    //             color="danger"
-    //             size="md"
-    //           >
-    //             Delete Map
-    //           </Button>
-    //         </Col>
-    //       </Row>
-    //     </Container>
-    //   </ModalBody>
-    // </Modal>
     <div className={styles.root}>
       <h1>Map Management Panel</h1>
+      <h2 className={`${styles['add-margin-bottom']}`}>Map Project Information</h2>
+      <div className={`${styles['add-margin-bottom']}`}>Name: TBD</div>
+      <div className={`${styles['add-margin-bottom']}`}>Description: TBD</div>
+      <div className={`${styles['add-margin-bottom']}`}>Saved Location: TBD</div>
+
+      <h2 className={`${styles['add-margin-bottom']}`}>Map Project Members</h2>
+      <ul>
+        <li>TBD</li>
+      </ul>
+
+      <h2 className={`${styles['add-margin-bottom']}`}>Public Access</h2>
+      <div className={`${styles['add-margin-bottom']}`}>Is-Public: {isPublic}</div>
+      <Button
+        style={{ width: '12rem' }}
+        className={`${styles['add-margin-bottom']}`}
+        color="success"
+        size="md"
+      >
+        Make Map Public
+      </Button>
+      <Button
+        style={{ width: '12rem' }}
+        className={`${styles['add-margin-bottom']}`}
+        color="danger"
+        size="md"
+      >
+        Delete Map
+      </Button>
     </div>
   );
 };
 
-export default ManageMapProjectModal;
+export default ManageMapProjectPanel;
