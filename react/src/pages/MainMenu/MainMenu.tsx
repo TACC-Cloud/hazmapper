0d4c2935db99698b44f9385b6f729f7ec5033c05
sophia-massie <smassie@tacc.utexas.edu>
2024-11-14 20:07:46 -0600

    - Moved assets to public - Created Main Menu layout similar to prod

--- react/src/pages/MainMenu/MainMenu.tsx
+++ react/src/pages/MainMenu/MainMenu.tsx
@@ -8,6 +8,8 @@ import {
 import useAuthenticatedUser from '@hazmapper/hooks/user/useAuthenticatedUser';
 import { SystemSelect } from '@hazmapper/components/Systems';
 import { ProjectListing } from '@hazmapper/components/Projects/ProjectListing';
+import { Layout } from 'antd';
+import styles from './layout.module.css';
 
 const MainMenu = () => {
   const {
@@ -37,18 +39,37 @@ const MainMenu = () => {
     setSelectedSystem(system);
   };
 
+  const headerStyle = {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    display: 'flex',
+  };
+
   return (
-    <>
-      <SectionHeader isNestedHeader>Main Menu</SectionHeader>
-      <div>
-        <InlineMessage type="info">
-          Welcome, {userData?.username || 'User'} <Icon name="user"></Icon>
-        </InlineMessage>
-      </div>
-      <ProjectListing />
-      {selectedSystem && <div>Current system selected: {selectedSystem}</div>}
-      <SystemSelect onSystemSelect={handleSelectChange}></SystemSelect>
-    </>
+    <div className={styles.root}>
+      <Layout.Header style={headerStyle}>
+        <img width="150px" src="./assets/hazmapper-header-logo.png" />
+        <div className={styles.userName}>{userData.username}</div>
+      </Layout.Header>
+      <Layout.Content>
+        <ProjectListing />
+        {selectedSystem && <div>Current system selected: {selectedSystem}</div>}
+        <SystemSelect onSystemSelect={handleSelectChange}></SystemSelect>
+      </Layout.Content>
+      <Layout.Footer>
+        <div className={styles.logoContainer}>
+          <a href="https://www.nsf.gov/">
+            <img src="./assets/nsf.png" width="60px" />
+          </a>
+          <a href="https://www.designsafe-ci.org/">
+            <img src="./assets/designsafe.svg" width="200px" />
+          </a>
+          <a href="https://www.designsafe-ci.org/about/">
+            <img src="./assets/nheri.png" width="150px" />
+          </a>
+        </div>
+      </Layout.Footer>
+    </div>
   );
 };
 
