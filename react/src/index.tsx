4cad5817d28705b2a76191e6a81bcd795b09af34
Sal Tijerina <r.sal.tijerina@gmail.com>
2025-01-08 15:54:29 -0600

    task/WG-394: Global Antd styling (#298)
    * upgrade antd
    
    * add global antd styles and common componentsg

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -6,12 +6,39 @@ import './index.css';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import { queryClient } from './queryClient';
+import { ConfigProvider, ThemeConfig } from 'antd';
+
+const themeConfig: ThemeConfig = {
+  token: {
+    borderRadius: 0,
+    colorPrimary: '#337ab7',
+    colorError: '#d9534f',
+    colorPrimaryTextHover: 'black',
+    colorBorderSecondary: '#b7b7b7',
+  },
+  components: {
+    Table: {
+      cellPaddingBlock: 8,
+      headerBg: 'transparent',
+      headerColor: '#333333',
+      headerSplitColor: 'transparent',
+      rowHoverBg: 'rgb(230, 246, 255)',
+      borderColor: 'rgb(215, 215, 215)',
+      colorText: 'rgb(112, 112, 112)',
+    },
+    Layout: {
+      bodyBg: 'transparent',
+    },
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ConfigProvider theme={themeConfig}>
+          <App />
+        </ConfigProvider>
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>
