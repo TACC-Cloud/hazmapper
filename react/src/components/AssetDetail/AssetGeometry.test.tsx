4c26a0b80e2fb7436bf4b97c5f66e34360ff6fee
Nathan Franklin <nathan.franklin@gmail.com>
2025-03-31 23:58:53 +0200

    Hotfix/length calculation and point lat lon (#348)
    * Fix lat/long swap on point display and length calculation
    
    * Remove deprecated and unused meta
    
    * Remove unneeded comments

--- react/src/components/AssetDetail/AssetGeometry.test.tsx
+++ react/src/components/AssetDetail/AssetGeometry.test.tsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AssetGeometry from './AssetGeometry';
-import { mockImgFeature } from '@hazmapper/__fixtures__/featuresFixture';
-
-jest.mock('@turf/turf', () => ({
-  ...jest.requireActual('@turf/turf'),
-  area: jest.fn(() => 1234.56),
-  length: jest.fn(() => 5678.9),
-  bbox: jest.fn(() => [10, 20, 30, 40]),
-}));
+import {
+  mockImgFeature,
+  mockLineFeature,
+  mockPolygonFeature,
+} from '@hazmapper/__fixtures__/featuresFixture';
 
 jest.mock('@hazmapper/hooks', () => ({
   useFeatureSelection: jest.fn(),
 }));
 
 describe('AssetGeometry', () => {
-  const GeometryAssetProps = {
-    selectedFeature: mockImgFeature,
-  };
-
   it('renders geometry for point on an image', () => {
-    const { getByText } = render(<AssetGeometry {...GeometryAssetProps} />);
+    const { getByText, queryByText } = render(
+      <AssetGeometry selectedFeature={mockImgFeature} />
+    );
     expect(getByText('Geometry: Point')).toBeDefined();
     expect(getByText('Latitude')).toBeDefined();
+    expect(getByText('Latitude').nextElementSibling?.textContent).toBe(
+      '30.389785554'
+    );
     expect(getByText('Longitude')).toBeDefined();
+    expect(getByText('Longitude').nextElementSibling?.textContent).toBe(
+      '-97.726075437'
+    );
+    expect(queryByText('Bounding Box')).toBeNull();
+  });
+
+  it('renders line feature with length', () => {
+    const { getByText } = render(
+      <AssetGeometry selectedFeature={mockLineFeature} />
+    );
+    expect(getByText('Geometry: Line String')).toBeDefined();
+    expect(getByText('Length (m)')).toBeDefined();
+    expect(getByText('Length (m)').nextElementSibling?.textContent).toBe(
+      '120.42'
+    );
+    expect(getByText('Bounding Box')).toBeDefined();
+  });
+
+  it('renders polygon feature with area', () => {
+    const { getByText } = render(
+      <AssetGeometry selectedFeature={mockPolygonFeature} />
+    );
+    expect(getByText('Geometry: Polygon')).toBeDefined();
+    expect(getByText('Area (m²)')).toBeDefined();
+    expect(getByText('Area (m²)').nextElementSibling?.textContent).toBe(
+      '4917.50'
+    );
+    expect(getByText('Bounding Box')).toBeDefined();
   });
 });
