d07c0cedc63ed0c112bc76519ebe4f7c30b9d9fe
Sal Tijerina <r.sal.tijerina@gmail.com>
2025-08-18 17:47:32 -0500

    update tapis token in localstorage via websocket

--- react/src/hooks/notifications/useGeoapiNotifications.ts
+++ react/src/hooks/notifications/useGeoapiNotifications.ts
@@ -1,72 +1,97 @@
 import { useEffect } from 'react';
 import useWebSocket from 'react-use-websocket';
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, QueryClient } from '@tanstack/react-query';
 import {
   KEY_USE_FEATURES,
   KEY_USE_POINT_CLOUDS,
   KEY_USE_TILE_SERVERS,
+  useAuthenticatedUser,
 } from '@hazmapper/hooks';
 import { useNotification } from './useNotification';
+import { NotificationAPI } from '@hazmapper/context/NotificationProvider';
+import { AuthState } from '@hazmapper/types';
+import {
+  setAuthenticatedUserFromLocalStorage,
+  removeAuthenticatedUserFromLocalStorage,
+} from '@hazmapper/utils/authUtils';
 
 type WebsocketNotification = {
-  id: number;
-  user: string;
-  tenant_id: string;
-  created: string;
   status: 'success' | 'warning' | 'error';
   message: string;
-  viewed: boolean;
+};
+
+const processNotification = (
+  data: WebsocketNotification,
+  queryClient: QueryClient,
+  notification: NotificationAPI
+) => {
+  notification[data.status]({
+    description: data.message,
+  });
+
+  if (data.status === 'success') {
+    // we assume that if some action was updated we need to refresh things so
+    // we invalidate relevant queries.
+    //  Note we are doing a force refetch as makes unique KEY_USE_FEATURES work
+
+    Promise.all(
+      [KEY_USE_FEATURES, KEY_USE_POINT_CLOUDS, KEY_USE_TILE_SERVERS].map(
+        (key) =>
+          queryClient
+            .invalidateQueries({
+              queryKey: [key],
+              refetchType: 'all',
+            })
+            .then(() => {
+              const queries = queryClient.getQueriesData({
+                queryKey: [key],
+              });
+              // Force refetch active queries for each key
+              return Promise.all(
+                queries.map(([queryKey]) =>
+                  queryClient.refetchQueries({
+                    queryKey,
+                    type: 'active',
+                    exact: true,
+                  })
+                )
+              );
+            })
+      )
+    );
+  }
 };
 
 export const useGeoapiNotifications = () => {
   const queryClient = useQueryClient();
   const notification = useNotification();
+  const { username } = useAuthenticatedUser();
   const { lastMessage } = useWebSocket('ws://localhost:8000/ws');
 
   useEffect(() => {
     if (lastMessage !== null) {
-      let data: WebsocketNotification;
+      let data: WebsocketNotification | AuthState;
       try {
         data = JSON.parse(lastMessage.data);
       } catch {
         console.error('Failed to parse WebSocket message:', lastMessage.data);
         return;
       }
-      notification[data.status]({
-        description: data.message,
-      });
 
-      if (data.status === 'success') {
-        // we assume that if some action was updated we need to refresh things so
-        // we invalidate relevant queries.
-        //  Note we are doing a force refetch as makes unique KEY_USE_FEATURES work
-
-        Promise.all(
-          [KEY_USE_FEATURES, KEY_USE_POINT_CLOUDS, KEY_USE_TILE_SERVERS].map(
-            (key) =>
-              queryClient
-                .invalidateQueries({
-                  queryKey: [key],
-                  refetchType: 'all',
-                })
-                .then(() => {
-                  const queries = queryClient.getQueriesData({
-                    queryKey: [key],
-                  });
-                  // Force refetch active queries for each key
-                  return Promise.all(
-                    queries.map(([queryKey]) =>
-                      queryClient.refetchQueries({
-                        queryKey,
-                        type: 'active',
-                        exact: true,
-                      })
-                    )
-                  );
-                })
-          )
+      if ((data as WebsocketNotification).message) {
+        processNotification(
+          data as WebsocketNotification,
+          queryClient,
+          notification
         );
+      } else if ((data as AuthState).authToken) {
+        // If backend sends auth state in websocket, update local storage auth state.
+        // This can be removed once auth state is no longer tracked in local storage.
+        if ((data as AuthState).user?.username !== username) {
+          removeAuthenticatedUserFromLocalStorage();
+        }
+        setAuthenticatedUserFromLocalStorage(data as AuthState);
       }
     }
-  }, [lastMessage, notification, queryClient]);
+  }, [lastMessage, queryClient, notification, username]);
 };
