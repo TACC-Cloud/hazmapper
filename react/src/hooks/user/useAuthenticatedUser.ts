3d2833d68b87739488d2252b0d5c383cd93e9f56
Sal Tijerina <r.sal.tijerina@gmail.com>
2025-10-16 15:54:35 -0500

    support unique csrftoken per env

--- react/src/hooks/user/useAuthenticatedUser.ts
+++ react/src/hooks/user/useAuthenticatedUser.ts
@@ -2,7 +2,7 @@ import { AxiosError } from 'axios';
 import { isTokenValid } from '@hazmapper/utils/authUtils';
 import { useSuspenseQuery, queryOptions } from '@tanstack/react-query';
 import { getApiClient } from '@hazmapper/requests';
-import { AuthState, AuthToken } from '@hazmapper/types';
+import { AuthState, AuthToken, ApiService } from '@hazmapper/types';
 import { useAppConfiguration } from '@hazmapper/hooks';
 
 export type TAuthenticatedUserResponse = {
@@ -10,8 +10,8 @@ export type TAuthenticatedUserResponse = {
   authToken: AuthToken | null;
 };
 
-async function getAuthenticatedUser(baseUrl: string) {
-  const apiClient = getApiClient();
+async function getAuthenticatedUser(baseUrl: string, geoapiEnv: string) {
+  const apiClient = getApiClient(ApiService.Geoapi, geoapiEnv);
   const endpoint = `${baseUrl}/auth/user/`;
   try {
     const res = await apiClient.get<TAuthenticatedUserResponse>(endpoint);
@@ -25,10 +25,10 @@ async function getAuthenticatedUser(baseUrl: string) {
   }
 }
 
-export const getAuthenticatedUserQuery = (baseUrl: string) =>
+export const getAuthenticatedUserQuery = (baseUrl: string, geoapiEnv: string) =>
   queryOptions({
     queryKey: ['authenticated-user'],
-    queryFn: () => getAuthenticatedUser(baseUrl),
+    queryFn: () => getAuthenticatedUser(baseUrl, geoapiEnv),
     staleTime: 1000 * 60 * 60 * 4 - 1000 * 60 * 5, // 3hrs 55 minutes stale time
     refetchInterval: 1000 * 60 * 30, // Refetch every 30 minutes
     refetchIntervalInBackground: true,
@@ -44,8 +44,8 @@ export const getAuthenticatedUserQuery = (baseUrl: string) =>
   });
 
 function useAuthenticatedUser() {
-  const { geoapiUrl } = useAppConfiguration();
-  return useSuspenseQuery(getAuthenticatedUserQuery(geoapiUrl));
+  const { geoapiUrl, geoapiEnv } = useAppConfiguration();
+  return useSuspenseQuery(getAuthenticatedUserQuery(geoapiUrl, geoapiEnv));
 }
 
 export default useAuthenticatedUser;
